c512fdd9d691ebda50f3a84d29827a7613492320
YiJun <cloudywolf1@qq.com>
2020-02-17 15:35:54 +0800

    fix: set undefined value

--- src/jsform/index.js
+++ src/jsform/index.js
@@ -337,6 +337,6 @@ JsForm.getPluginList = ()=> JsFormPlugins.getPluginList();
 JsForm.registerPlugin = plugin => JsFormPlugins.registerPlugin(plugin);
 JsForm.collect = (use, connect=false) => cubec.atom({ use: _isString(use) ? [use] : (_isArrayLike(use) ? use : []), connect });
 
-JsForm.verison = "0.0.13";
+JsForm.verison = "0.0.14";
 
 export default JsForm;
