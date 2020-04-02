31ede5e79a69f3206a3918265b6109180110d800
YiJun <cloudywolf1@qq.com>
2020-04-02 23:25:25 +0800

    fix: char version 0.0.20

--- src/jsform/index.js
+++ src/jsform/index.js
@@ -415,7 +415,7 @@ _eachArray([
 JsForm.getPluginList = ()=> JsFormPlugins.getPluginList();
 JsForm.registerPlugin = plugin => JsFormPlugins.registerPlugin(plugin);
 JsForm.collect = (use, connect=false) => cubec.atom({ use: _isString(use) ? [use] : (_isArrayLike(use) ? use : []), connect });
-JsForm.verison = "0.0.2";
+JsForm.verison = "0.0.20";
 JsForm.author = "YiJun";
 
 export default JsForm;
